d2ed0fd3d1052457180c1180acb5806791d70f4d
Daniel Sanchez <danielsanchez1502@gmail.com>
2021-11-12 17:07:15 -0300

    Adjustments to Foldes in Repo

---
+++ frontend/NextJS-Demo/hello-world/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next.Js Web APP</title>
+        <meta name="NextJs" content="Hello World Title with Next.js" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Hello <a href="https://nextjs.org">World with Next.JS!</a>
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <Link href="/" target="_blank">Created by dannzdev ✌️</Link>
+      </footer>
+    </div>
+  )
+}

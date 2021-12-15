c20561d4c0b34319991f35207aedf6c3bb4fa632
Daniel Sanchez <danielsanchez1502@gmail.com>
2021-12-14 23:52:12 -0300

    update16

--- frontend/NextJS-Demo/hello-world/pages/index.js
+++ frontend/NextJS-Demo/hello-world/pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          I'm the MVP of Platzily
+          I am the MVP of Platzily
         </h1>
       </main>
 
